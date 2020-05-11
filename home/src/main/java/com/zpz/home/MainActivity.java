c7675f4f7227903c9f566ecb2611d57d04e1ff00
Xyf6 <15159862589@163.com>
2020-05-11 17:53:17 +0800

    Initial commit

---
+++ home/src/main/java/com/zpz/home/MainActivity.java
@@ -0,0 +1,25 @@
+package com.zpz.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zpz.common.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+    }
+
+    @Override
+    public void fillData() {
+
+    }
+}
---
+++ main/src/main/java/com/zpz/main/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.zpz.main;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zpz.common.base.BaseActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+
+}
